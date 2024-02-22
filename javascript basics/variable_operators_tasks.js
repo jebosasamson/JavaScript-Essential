0175071ca88bcf2e0cb196c8fa67e5e711db2a3f
Jebosa Samson <Jebosasamson7@gmail.com>
2024-02-22 06:20:23 -0800

    variable operator exe

---
+++ javascript basics/variable_operators_tasks.js
@@ -0,0 +1,20 @@
+// Task 1: Declare two variables x and y, and assign them values. Print
+// the sum of these two variables.
+
+let x = 10 
+let y = 30
+
+let sum = x + y
+console.log(sum)
+
+
+// Declare a variable num1 and assign it a numeric value. Declare
+// another variable str1 and assign it a string value. Concatenate
+// num1 and str1, and print the result.
+
+let num1 = 25
+let str1 = '10'
+
+let print = num1 + 'str1'
+
+console.log(print)
